a64b690b519a85e50f65d67ce0517c2d043ce83a
Alexander Franks <afranks@gmail.com>
2022-12-09 13:27:48 -0800

    Updpated simulation settings and works with utility functions

--- utility_functions.R
+++ utility_functions.R
@@ -56,17 +56,16 @@ r2_to_odds <- function(r2, probs=NULL, alpha=0.95) {
   odds_ratio
 }
 
-cal_tau_cali_wNC <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_list, 
+compute_bounds_and_robustness <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_list=data_list, 
                              treatment_index=1, 
                              marginal_sensitivity_param=FALSE, probs=NULL, 
                              full_rank=FALSE,
                              r2y=NULL, sdy=1) {
 
 
-  sigma_t_hat <- sd(data_list$x[, treatment_index])
+  sigma_t_hat <- sd(unlist(data_list$x[, treatment_index]))
 
   m <- ifelse(full_rank, data_list$K, data_list$M)
-
   beta_naive <- df %>% filter(M==1, J==treatment_index) %>% pull(beta) * sdy
   
   if(full_rank) {
@@ -76,7 +75,7 @@ cal_tau_cali_wNC <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_li
     lambda <- df %>% 
       filter(J==treatment_index) %>% 
       filter(M==1) %>% 
-      pull(lambda) %>% diag
+      pull(lambda) %>% diag * sdy^2
     
       BB <- B %*% t(B)
       if(!is.null(r2y)){
@@ -87,17 +86,19 @@ cal_tau_cali_wNC <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_li
       }
 
       eig_gamma <- eigen(B %*% t(B) + lambda_scaler*lambda)
-      Gamma_tilde <- eig_gamma$vectors %*% diag(sqrt(abs(eig_gamma$values))) * sdy
+      Gamma_tilde <- eig_gamma$vectors %*% diag(sqrt(abs(eig_gamma$values)))  * sdy
       lambda <- lambda - lambda_scaler*lambda
     
   } else {
-    Gamma_tilde <- df %>% filter(J==treatment_index) %>% pull(B) %>% matrix(dat=., nrow=data_list$K, ncol=m, byrow=TRUE) * sdy
+    Gamma_tilde <- df %>% filter(J==treatment_index) %>% pull(B) %>% 
+      matrix(dat=., nrow=data_list$K, ncol=m, byrow=TRUE) * sdy
     lambda <- df %>% 
       filter(J==treatment_index) %>% 
       filter(M==1) %>% 
-      pull(lambda) * sdy %>% diag
+      pull(lambda) %>% diag * sdy^2
   }
 
+  var_y <- diag(Gamma_tilde %*% t(Gamma_tilde) + lambda)
   
   Gamma_c <- matrix(Gamma_tilde[NC_index,], ncol = m)
   Gamma_c_inv <- MASS::ginv(Gamma_c)
@@ -116,7 +117,7 @@ cal_tau_cali_wNC <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_li
   R2_min_nc <- sigma_t_hat^2*sum((Gamma_c_inv %*% beta_naive_nc)^2) / 
     (1 + sigma_t_hat^2*sum((Gamma_c_inv %*% beta_naive_nc)^2))
   # beta_nc <- sigma_t_hat^2*sqrt(1-R2)*Gamma_c_inv %*% tau_naive_c
-    
+
   bias_no_nc <- (sqrt(apply(Gamma_tilde, 1, function(x) sum(x^2))) / sigma_t_hat) * sqrt(R2_no_nc/(1-R2_no_nc))
   bias_nc <- c(Gamma_tilde %*% Gamma_c_inv %*% beta_naive_nc)
   r2_less_than_min <- FALSE
@@ -125,6 +126,7 @@ cal_tau_cali_wNC <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_li
     R2_nc <- R2_min_nc + 1e-9
     r2_less_than_min <- TRUE
   }
+  
   
   if(ncol(MASS::Null(t(Gamma_c))) == 0){
     cat("PATEs are identifiable.")
@@ -145,7 +147,6 @@ cal_tau_cali_wNC <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_li
     
     bias_ncperp[abs(bias_ncperp) < 1e-12] <- 0
 
-
     est_df <- cbind(beta_naive, beta_naive - bias_no_nc, beta_naive + bias_no_nc, 
                     beta_naive - bias_nc, beta_naive - bias_nc + bias_ncperp)
     colnames(est_df) <- c("Naive", "R2_lwr", "R2_upr", "R2_nc_0", "R2_nc_lwr", "R2_nc_upr")
@@ -160,6 +161,7 @@ cal_tau_cali_wNC <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_li
     rv <- rv_mediate / (1 + rv_mediate)
     est_df$rv_init <- rv_init
     est_df$rv_nc <- sign(debiased+1e-16)*rv
+    est_df$rv_nc_cond <- (est_df$rv_nc - rv_init)/(1-rv_init)
     
     est_df$K <- 1:data_list$K
     est_df$r2_less_than_min <- r2_less_than_min
@@ -169,16 +171,27 @@ cal_tau_cali_wNC <- function(df, R2_no_nc=0.0, R2_nc=R2_no_nc, NC_index, data_li
     est_df$lambda <- diag(lambda)
     est_df$total_var <- diag(Gamma_tilde %*% t(Gamma_tilde)) + diag(lambda)
     
+    ## C&H RV and XRV
+    b <- beta_naive^2 * (sigma_t_hat^2/var_y)
+    c <- -b
+    a <- 1
+    rv_ch <- (-b + sqrt(b^2 - 4*a*c))/(2*a)
+    xrv <- b / (1+b)
+    
+    est_df$rv_ch <- rv_ch
+    est_df$xrv <- xrv
+    
     if(marginal_sensitivity_param) {
       if(is.null(probs))
         stop("Must specify probs when marginal_sensitivity_param is TRUE.")
 
-
+      
       est_df$odds_init <- r2_to_odds(est_df$rv_init, probs=probs)
       est_df$odds_nc <- r2_to_odds(est_df$rv_nc, probs=probs)
-      
+      est_df$odds_ch <- r2_to_odds(est_df$rv_ch, probs=probs)
+      est_df$odds_xrv <- r2_to_odds(est_df$xrv, probs=probs)
     }
-    
+
     est_df
   }
  
@@ -200,7 +213,7 @@ compute_r2y <- function(stan_results) {
 
 get_beta_w_norm <- function(stan_results, data_list, NC_index = 1,
                 treatment_index=1, marginal_sensitivity_param = TRUE,
-                probs=NULL, full_rank=FALSE, R2_no_nc=0.0, R2_nc=R2_no_nc, r2y=NULL) {
+                probs=NULL, full_rank=FALSE, R2_no_nc=0.0, R2_nc=R2_no_nc, r2y=NULL, sdy=1) {
 
   stan_results %>% spread_draws(B[K, M], lambda[K], beta[K, J]) %>% 
     group_by(.draw) %>% 
@@ -208,10 +221,10 @@ get_beta_w_norm <- function(stan_results, data_list, NC_index = 1,
   
   beta_w_norm <- nest_df %>% 
     mutate(data = purrr::map(data, function(
-      d) cal_tau_cali_wNC(d, R2_no_nc=R2_no_nc, R2_nc=R2_nc, NC_index=NC_index, 
-                          treatment_index = 1, data_list=data_list, 
+      d) compute_bounds_and_robustness(d, R2_no_nc=R2_no_nc, R2_nc=R2_nc, NC_index=NC_index, 
+                          treatment_index = treatment_index, data_list=data_list, 
                           marginal_sensitivity_param = marginal_sensitivity_param, 
-                          probs=probs, full_rank=full_rank, r2y=r2y))) %>%
+                          probs=probs, full_rank=full_rank, r2y=r2y, sdy=sdy))) %>%
     unnest(cols = c(data))
   
 }
@@ -228,6 +241,8 @@ compute_effect_intervals <- function(beta_w_norm) {
                               lower_rv_nc = quantile(abs(rv_nc), 0.025, na.rm=TRUE),
                               lower_odds_init = quantile(abs(odds_init), 0.025, na.rm=TRUE),
                               lower_odds_nc = quantile(abs(odds_nc), 0.025, na.rm=TRUE),
+                              lower_odds_ch = quantile(abs(odds_ch), 0.025, na.rm=TRUE),
+                              lower_odds_xrv = quantile(abs(odds_xrv), 0.025, na.rm=TRUE),
     ) %>% 
     mutate(Metabolite = colnames(Y)) %>%
     mutate(Metabolite = fct_relevel(Metabolite, 
@@ -250,13 +265,26 @@ make_interval_plot <- function(effect_intervals, nc_plot_index=1, title="",
                      y=lower_naive, yend=upper_naive, col='nuc'), size=1.5, alpha=0.75) +
     geom_rect(aes(xmin=as.numeric(forcats::fct_inorder(Metabolite))-0.1, xmax=as.numeric(forcats::fct_inorder(Metabolite))+0.1, 
                      ymin=lower_init, ymax=upper_init, col="rect"), fill=alpha("white", 0)) +
+    geom_text(aes(x=forcats::fct_inorder(Metabolite), y = rep(2, 10),
+              label=ifelse(sign(lower_naive) == sign(upper_naive), format(round(lower_odds_ch, digits=1), nsmall=1), "X")), fontface="bold") +    
+    geom_text(aes(x=forcats::fct_inorder(Metabolite), y = rep(1.8, 10),
+              label=ifelse(sign(lower_naive) == sign(upper_naive), format(round(lower_odds_xrv, digits=1), nsmall=1), "X")), fontface="bold") +
     geom_text(aes(x=forcats::fct_inorder(Metabolite), y = rep(1.6, 10),
                   label=ifelse(sign(lower_naive) == sign(upper_naive), format(round(lower_odds_init, digits=1), nsmall=1), "X")), fontface="bold") +
     geom_text(aes(x=forcats::fct_inorder(Metabolite), y = rep(1.4, 10),
-                  label=ifelse(sign(lower_nc) == sign(upper_nc), format(round(lower_odds_nc, digits=1), nsmall=1), "X")), col=nc_color, fontface="bold") +
+                  label=ifelse(sign(lower_nc) == sign(upper_nc), format(round(lower_odds_nc, digits=1), nsmall=1), "")), col=nc_color, fontface="bold", nudge_x=.3) +
+    geom_text(aes(x=forcats::fct_inorder(Metabolite), y = rep(1.4, 10),
+                  label=ifelse(sign(lower_nc) == sign(upper_nc), format(round(lower_odds_nc / lower_odds_init[nc_plot_index], digits=1), nsmall=1), "")), 
+              col=nc_color, fontface="bold", nudge_x=-.25) +
+    geom_text(aes(x=forcats::fct_inorder(Metabolite), y = rep(1.4, 10),
+              label=ifelse(sign(lower_nc) == sign(upper_nc), "/", "X")), col=nc_color, fontface="bold") +
+    
+    geom_text(data=tibble(), aes(label=c(as.character(expression(RV["NC"]^Gamma)), as.character(expression(RV^Gamma)), "XRV", as.character(expression(RV^1))), 
+                                 x=rep(-.2, 4), y=seq(1.4, 2, by=0.2)), 
+              fontface="bold", hjust=0, col="dark blue", parse=TRUE) +
     geom_point(aes(x=nc_plot_index, y=rep(0, length(nc_plot_index))), size=3, colour=nc_color, alpha=0.5, data=tibble()) +
     theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 45, hjust=1)) + 
-    xlab("") + ylab("Treatment Effect") + 
+    xlab("") + ylab("Treatment Effect") +  expand_limits(x=-.35) +
     geom_hline(yintercept=0, linetype="dashed") +
     # scale_color_manual(name="", labels=labels, 
     #                    values=c(nuc="black", 'null'=nc_color)) + 
